26ee4e40bf25f9ae88e7632c7c13e8e16e022816
DESKTOP-918846P\pervolo <d.nalyvaiko@pervolo.com>
2017-02-22 17:08:05 +0200

    Linq

---
+++ Trest/Trest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Trest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] NewFile = File.ReadAllLines("sample.txt");
+           IEnumerable<string> numbers = ( NewFile.Where(x => new Regex(@"^\d+$").IsMatch(x) )).ToList();
+
+
+            IEnumerable<int> number = numbers.Select(x=>Int32.Parse(x)).ToList();
+
+            foreach (string k in numbers)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine("Sum: " + number.Sum());
+            Console.WriteLine("Average: " + number.Average());
+            Console.WriteLine("MIn: "+number.Min());
+            Console.WriteLine("Max: " + number.Max());
+            Console.ReadLine();
+        }
+    }
+}
