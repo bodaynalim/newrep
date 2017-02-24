e9906e1356383b0c466858b7f49066ffbcfb8c4b
DESKTOP-918846P\pervolo <d.nalyvaiko@pervolo.com>
2017-02-24 12:15:41 +0200

    MVVM

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -24,135 +24,18 @@ namespace WpfApplication1
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    class Download
-    {
-        public string Time { get; set; }
-        public string Site { get; set; }
-    }
+   
     public partial class MainWindow : Window
     {
+        #region Private Variables
+        private readonly ViewModel.DownloadViewModel _downloadViewModel;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            _downloadViewModel = new ViewModel.DownloadViewModel();
+            DataContext = _downloadViewModel;
         }
-
-        static string[] http = {"http://speedtest.newark.linode.com/100MB-newark.bin","http://speedtest.atlanta.linode.com/100MB-atlanta.bin",
-            "http://speedtest.frankfurt.linode.com/100MB-frankfurt.bin", "http://speedtest.singapore.linode.com/100MB-singapore.bin" };
-
-
-        static readonly string result = @"C:\Users\pervolo\AppData\Local\Temp\";
-    
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            listView.Items.Clear();
-            Disbuttons();
-            for (int i = 0; i < http.Length; i++)
-            {
-                func1(http[i]);
-                    
-            }
-
-        }
-
-        public void Enabledbuttons()
-        {
-            this.Dispatcher.Invoke(() =>
-            {
-                button.IsEnabled = true; button2.IsEnabled = true; button1.IsEnabled = true;
-            });
-        }
-
-        public void Disbuttons()
-        {
-            this.Dispatcher.Invoke(() =>
-            {
-                button1.IsEnabled = false; button.IsEnabled = false; button2.IsEnabled = false;
-            });
-        }
-
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            listView.Items.Clear();
-
-            for (int i = 0; i < http.Length; i++)
-            {
-                
-                new Thread(new ParameterizedThreadStart(func1)).Start(http[i]);
-              
-            }
-            
-
-        }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            listView.Items.Clear();
-            Disbuttons();
-            DisplayResultAsync();
-          
-        }
-
-
-      
-        static int number = 0;
-
-        void func1(object http)
-        {
-            Disbuttons();
-           
-            int numb = number;
-            number++;
-            WebClient client = new WebClient();
-            Stopwatch st = new Stopwatch();
-            st.Start();
-            client.DownloadFile((string)http, result + "myBook" + numb + ".bin");
-            st.Stop();
-            
-            listView.Dispatcher.Invoke(() =>
-            {
-                listView.Items.Add(new Download() { Time = st.Elapsed.ToString(), Site = (string)http });
-
-            });
-               
-           
-            File.Delete(result + "myBook" + numb + ".bin");
-
-            Enabledbuttons();
-           
-
-
-        }
-
-
-        async Task DisplayResultAsync()
-        {
-
-            Task t1 = Task.Run(() =>
-            {
-                func1(http[0]);
-            });
-            Task t2 = Task.Run(() =>
-            {
-                func1(http[1]);
-            }); ;
-            Task t3 = Task.Run(() =>
-            {
-                func1(http[2]);
-
-            });
-            Task t4 = Task.Run(() =>
-            {
-                func1(http[3]);
-
-            });
-
-
-            await Task.WhenAll(new[] { t1, t2, t3, t4 });
-
-          
-
-
-        }
+     
     }
 }
---
+++ WpfApplication1/WpfApplication1/View/Download.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Download : INotifyPropertyChanged
+    {
+        private string _time;
+        private string _site;
+
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged("Time");
+            }
+        }
+
+        public string Site
+        {
+            get { return _site; }
+            set
+            {
+                _site = value;
+                OnPropertyChanged("Site");
+            }
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
