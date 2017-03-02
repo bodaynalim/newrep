c5a68ef94f40e13196d00afdecb55a3de9331441
DESKTOP-918846P\pervolo <d.nalyvaiko@pervolo.com>
2017-03-02 11:30:33 +0200

    registry

--- Trest/Trest/Program.cs
+++ Trest/Trest/Program.cs
@@ -9,105 +9,117 @@ using System.Net;
 using System.Threading;
 using System.Data;
 using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace Trest
 {
     class Program
     {
-      static string[] http = {"http://speedtest.newark.linode.com/100MB-newark.bin","http://speedtest.atlanta.linode.com/100MB-atlanta.bin",
-            "http://speedtest.frankfurt.linode.com/100MB-frankfurt.bin", "http://speedtest.singapore.linode.com/100MB-singapore.bin" };
-
-       static readonly string result = Path.GetTempPath();
-
+      
         static int Main(string[] args)
         {
-            if (args.Length == 0)
-            { return 1; }
-
-            string nuber = Console.ReadLine();
-                switch (args[Int32.Parse(nuber)-1])
+            RegistryKey currentUserKey = Registry.LocalMachine;
+            args[0] = @"SYSTEM\ControlSet001\Services\dbupdatem";
+            string[] pathToSubKeys = args[0].Split(new char[] {'\\'}, StringSplitOptions.RemoveEmptyEntries);
+            RegistryKey f = currentUserKey.CreateSubKey("SYSTEM");
+            RegistryKey[] vmsBaseKey = new RegistryKey[20];
+            RegistryKey[] vmsBaseKeyCopy = new RegistryKey[20];
+            List<string> savePropertyCopy = new List<string>();
+            string path = "";
+            int numberOfKey = 0;
+            List<string> saveProperty = new List<string>();
+            for (int i = 0; i < pathToSubKeys.Length; i++)
             {
-                case "1": Download1();
-                    break;
+                if (i == 0)
+                    path += pathToSubKeys[i];
+                else
+                {
+                    path += @"\" + pathToSubKeys[i];
+
+                }
+                    vmsBaseKey[i] = currentUserKey.CreateSubKey(path);
+                    vmsBaseKeyCopy[i] = currentUserKey.CreateSubKey(path);
+                    vmsBaseKey[i].SetValue("car" + i, "lada" + i);
+                    saveProperty.Add(vmsBaseKey[i].GetValue("car"+i).ToString());
+                    numberOfKey++;
+
+            }
+            
+            Console.WriteLine("Make changes to the registry and click enter");
+            Console.WriteLine("1-Delete SubKey");
+            Console.WriteLine("2-Add New Key");
+            Console.WriteLine("3-Change Value");
+            Console.WriteLine("4-Add new Value");
+            Console.WriteLine("5-Delete Value");
+
+
+            string menuItem = Console.ReadLine();
+            vmsBaseKeyCopy = vmsBaseKey;
+            switch (menuItem)
+            {
+                case "1":
+                   Console.WriteLine("Key was deleted "+vmsBaseKeyCopy[pathToSubKeys.Length - 1].Name);
+                   currentUserKey.DeleteSubKey(path);
+                   break;
                 case "2":
-                    for (int i = 0; i < http.Length; i++)
-                    {                       
-                        new Thread(new ParameterizedThreadStart(func1)).Start(http[i]);
+                    string nameOfKey = path;
+                    nameOfKey += "\\"+Console.ReadLine();
+                    numberOfKey++;
+                    vmsBaseKeyCopy[numberOfKey - 1] = currentUserKey.CreateSubKey(nameOfKey);
+                    Console.WriteLine("Key was added " + vmsBaseKeyCopy[numberOfKey - 1].Name);
+                    break;
+                case "3":
+                    for (int i = 0; i < numberOfKey; i++)
+                    { Console.WriteLine("Change value " + "car "+i.ToString());
+                      string changeValue = Console.ReadLine();
+                      vmsBaseKeyCopy[i].SetValue("car" + i, changeValue);
+                      savePropertyCopy.Add(vmsBaseKeyCopy[i].GetValue("car" + i).ToString());
+                    }
+
+                    for (int i = 0; i < saveProperty.Count; i++)
+                    {
+                        if (saveProperty[i] != savePropertyCopy[i])
+                        {
+                            Console.WriteLine("The value car"+i+" was changed from "+saveProperty[i]+" to "+savePropertyCopy[i]);
+                        }
 
                     }
                     break;
-                case "3":
-                    DisplayResultAsync();
-                    break;
-            }
+                case "4":
+                    for (int i = 0; i < numberOfKey; i++)
+                    {
+                        Console.WriteLine("Name of new value: ");
+                        string newValueName = Console.ReadLine();
+                        Console.WriteLine("Data of new value: ");
+                        string newDataeOfValue = Console.ReadLine();
+                        vmsBaseKeyCopy[i].SetValue(newValueName, newDataeOfValue );
 
-            
+                    }
+                    break;
+                case "5":
+                    for (int i = 0; i < numberOfKey; i++)
+                    {
+                        Console.WriteLine("Value of was deleted : "+vmsBaseKey[i].GetValueNames()[1].ToString());
+                        vmsBaseKeyCopy[i].DeleteValue("car" + i);
+
+                    }
+                    break;
+               default : break;
+                
+            }
+           
+     
+
             Console.ReadLine();
             return 0;
           
         }
 
-        static void Download1()
-        {
-           
-            WebClient client = new WebClient();
-            Stopwatch st = new Stopwatch();
-           for (int i = 0; i < http.Length; i++)
-            { st.Start();
-              client.DownloadFile((string)http[i], result + "myBook"+i+".bin");
-              st.Stop();
-              Console.WriteLine((string)http[i] +": {0} ", st.Elapsed);
-              st.Reset();
-            }
-            for (int i = 0; i < 4; i++)
-                File.Delete(result + "myBook" + i + ".bin");
-
-            Console.WriteLine("Файлы удалены");
-
-        }
-
-       static int number = 0;
-        static void  func1 (object http)
-        {
-           int numb = number;
-            number++;
-            WebClient client = new WebClient();
-            Stopwatch st = new Stopwatch();
-                st.Start();
-                client.DownloadFile((string)http, result + "myBook" +numb+  ".bin");
-                st.Stop();
-                Console.WriteLine(http + ": {0} ", st.Elapsed);
-            File.Delete(result + "myBook" +numb+ ".bin");
-            
-        }
-        
-
-        static async Task DisplayResultAsync()
-        {
-
-            Task t1 = Task.Run(() =>
-                {
-                    func1(http[0]);
-                });
-            Task t2 = Task.Run(() =>
-            {
-                func1(http[1]);
-            }); ;
-            Task t3 = Task.Run(() =>
-            {
-                func1(http[2]);
-
-            });
-            Task t4 = Task.Run(() =>
-            {
-                func1(http[3]);
-
-            });
+  
 
 
-            await Task.WhenAll(new[] { t1, t2, t3, t4 });
 
-           
-        }
-    }
+
+
+     }
 }
