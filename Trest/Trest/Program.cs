c4b68f7333b1ecddfb94828ae8d1e1a7ca7b1d32
DESKTOP-918846P\pervolo <d.nalyvaiko@pervolo.com>
2017-03-02 17:10:52 +0200

    new regestry1

--- Trest/Trest/Program.cs
+++ Trest/Trest/Program.cs
@@ -39,53 +39,113 @@ namespace Trest
 
     class Program
     {
-        
 
-
-
-        private RegistryKeyModel GetRegistryKey(string path)
+      static  private RegistryKeyModel GetRegistryKey(string path)
 
         {
-
             var result = new RegistryKeyModel
 
             {
                 Path = path
 
             };
-
-          
-            var key = Registry.LocalMachine.OpenSubKey(path, true);
             
+            var key = Registry.CurrentUser.OpenSubKey(path);
+
+            foreach (var valueName in key.GetValueNames())
+
+            {
+                result.Values.Add(new RegistryValue
+
+                {
+                    Name = valueName,
+                    Data = key.GetValue(valueName).ToString()
+
+                });
+                
+            }
             foreach (var subKeyName in key.GetSubKeyNames())
             {
-
+                
                 result.SubKeys.Add(GetRegistryKey(path + "\\" + subKeyName));
-
             }
-
-
-
+            
             return result;
 
         }
+
+        static private void SetNewValue(string path, int i)
+
+        {
+            var result = new RegistryKeyModel();
+
+          
+            var key = Registry.CurrentUser.OpenSubKey(path, RegistryKeyPermissionCheck.ReadWriteSubTree);
+            i++;
+            
+            key.SetValue("sfdfs" + i, "sdfsdf" + i);
+            foreach (var subKeyName in key.GetSubKeyNames())
+            {
+               SetNewValue(path + "\\" + subKeyName,i);
+            }
+
+          
+
+        }
+
+
+
+
+
         static int Main(string[] args)
         {
-            args[0] = @"SYSTEM\ControlSet001\Services";
-            string[] pathToSubKeys = args[0].Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
-            string path = "";
-            for (int i = 0; i < pathToSubKeys.Length; i++)
+            args[0] = @"System\CurrentControlSet\Control";
+            RegistryKeyModel keyNew;
+            keyNew =  GetRegistryKey(args[0]);
+
+            int j = 0;
+
+            SetNewValue(args[0],j);
+
+            RegistryKeyModel newKey1 = GetRegistryKey(args[0]);
+
+            if (keyNew.Values.Count == 0)
             {
-                if (i == 0)
-                    path += pathToSubKeys[i];
-                else
+                for (int i = 0; i < newKey1.Values.Count; i++)
                 {
-                    path += @"\" + pathToSubKeys[i];
-
+                    Console.WriteLine("New Value " + newKey1.Values[i].Name + newKey1.Values[i].Data);
                 }
-                new Program().GetRegistryKey(path);
-
             }
+            else if (keyNew.Values.Count == newKey1.Values.Count)
+            {
+                for (int i = 0; i < keyNew.Values.Count; i++)
+                
+                    if (keyNew.Values[i].Data != newKey1.Values[i].Data && keyNew.Values[i].Name != newKey1.Values[i].Name)
+                    {
+                        Console.WriteLine("Data an Name were changed" + newKey1.Values[i].Name + newKey1.Values[i].Data);
+                    }
+                    else if (keyNew.Values[i].Data == newKey1.Values[i].Data &&
+                             keyNew.Values[i].Name == newKey1.Values[i].Name)
+                    {
+                        Console.WriteLine("Nothing was changed");
+                    }
+                    else if (keyNew.Values[i].Data != newKey1.Values[i].Data &&
+                                 keyNew.Values[i].Name == newKey1.Values[i].Name)
+                    {
+                        Console.WriteLine("Data was changed" + newKey1.Values[i].Name + newKey1.Values[i].Data);
+                    }
+                    else if (keyNew.Values[i].Data == newKey1.Values[i].Data &&
+                                     keyNew.Values[i].Name != newKey1.Values[i].Name)
+                    {
+                        Console.WriteLine("Name of Value was changed" + newKey1.Values[i].Name);
+                    }
+            }
+                
+            
+
+            Console.ReadLine();
+
+
 
             return 0;
 
