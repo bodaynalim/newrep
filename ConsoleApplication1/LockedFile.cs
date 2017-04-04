b6b0f48746970c7e83fd809ca80bacf504d3f213
DESKTOP-918846P\pervolo <d.nalyvaiko@pervolo.com>
2017-04-04 12:45:15 +0300

    LockedFiles

---
+++ ConsoleApplication1/LockedFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class LockedFile
+    {
+        public LockedFile(string path, List<Process> getProcessById)
+        {
+            ChangedFile = path;
+            Processes = getProcessById;
+        }
+
+        public string ChangedFile { get; set; }
+        public List<Process> Processes { get; set; }
+
+    }
+}
